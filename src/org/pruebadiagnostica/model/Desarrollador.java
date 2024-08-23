f1c83056cda79972b18619693010aa0530b29ae5
David Morente <tu.email@example.com>
2024-08-23 16:45:25 -0600

    add:se creo y se agrego en el modelo Desarrollador

---
+++ src/org/pruebadiagnostica/model/Desarrollador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pruebadiagnostica.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Desarrollador extends Empleado {
+    private String lenguajeDeProgramacion;
+
+    public Desarrollador(String nombre, int edad, double salario, String lenguajeProgramacion) {
+        super(nombre, edad, salario);
+        this.lenguajeDeProgramacion = lenguajeProgramacion;
+    }
+
+    public void trabajar() {
+        System.out.println(nombre + " está desarrollando en " + lenguajeDeProgramacion);
+    }
+    
+    public void trabajar(String proyecto) {
+        System.out.println(nombre + " está trabajando en el proyecto: " + proyecto + " y usando el lenguaje de programación " + lenguajeDeProgramacion);
+    }
+}
+
+
--- src/org/pruebadiagnostica/model/Empleado.java
+++ src/org/pruebadiagnostica/model/Empleado.java
@@ -9,6 +9,48 @@ package org.pruebadiagnostica.model;
  *
  * @author informatica
  */
-public class Empleado {
+public abstract class Empleado {
+    String nombre;
+    private int edad;
+    private double salario;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
     
+    public abstract void trabajar();
+
+    public void trabajar(int horas) {
+        System.out.println(nombre + " trabaja " + horas + " horas.");
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
 }
